96727511e41d79f79de0a88adb5f850da4cf215e
Gordey <gordeylog71@gmail.com>
2023-01-10 16:05:34 +0300

    complet

---
+++ script.js
@@ -0,0 +1,7 @@
+const youAge = prompt('Введите ваш возраст');
+
+const result = youAge >= 18 ? ('Вы можете купить алкоголь') : ('Вы не можете купить алкоголь');
+
+alert(result);
+
+console.log(result);
